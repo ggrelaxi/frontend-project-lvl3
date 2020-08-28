008f3b505666a6108bd6b88aaf5072e56291fbda
ggrelaxi <ggrelaxi@gmail.com>
2020-08-28 09:53:18 +0300

    save state to try parsing refactor

--- src/app.js
+++ src/app.js
@@ -4,11 +4,63 @@ import _ from 'lodash';
 import watchedState from './watchers';
 
 const app = () => {
+  const corsServer = 'https://cors-anywhere.herokuapp.com/';
+
   const form = document.getElementById('rssForm');
+
+  const checkUpdate = (url, feedID) => {
+    const parser = new DOMParser();
+    const correctUrl = `${corsServer}${url}`;
+    axios.get(correctUrl)
+      .then((response) => {
+        const data = parser.parseFromString(response.data, 'text/xml');
+        const items = data.querySelectorAll('channel item');
+        const checkPosts = [];
+        [...items].forEach((item) => {
+          const link = item.querySelector('link').textContent;
+          const title = item.querySelector('title').textContent;
+          checkPosts.push({
+            feedId: feedID,
+            title: link,
+            link: title,
+          });
+        });
+        const actualPostLink = watchedState.posts.filter((post) => post.feedId === feedID);
+
+        const newLink = _.differenceWith(actualPostLink, checkPosts);
+        console.log(newLink)
+        if (newLink) {
+          
+        }
+        // [...items].forEach((singleElement) => {
+        //   const singleElementLink = singleElement.querySelector('link').textContent;
+        //   checkLink.push(singleElementLink);
+        //   const actualPost = watchedState.posts.filter((post) => post.feedId === feedID);
+        //   const alreadyAddLink = [];
+        //   actualPost.forEach((post) => alreadyAddLink.push(post.link));
+        //   checkLink.forEach((link) => {
+        //     const isNewLink = alreadyAddLink.includes(link);
+        //     if (!isNewLink) {
+        //       const singleElementTitle = singleElement.querySelector('title').textContent;
+        //       watchedState.posts.push({
+        //         id: _.uniqueId(),
+        //         feedId: feedID,
+        //         title: singleElementTitle,
+        //         link: singleElementLink,
+        //       });
+        //       watchedState.form.state = 'have update';
+        //       watchedState.form.state = '';
+        //       console.log(watchedState);
+        //     }
+        //   });
+        // });
+      });
+    setTimeout(() => checkUpdate(url, feedID), 5000);
+  };
 
   const downloadRss = (url) => {
     const parser = new DOMParser();
-    const correctUrl = `https://cors-anywhere.herokuapp.com/${url}`;
+    const correctUrl = `${corsServer}${url}`;
     const feedID = _.uniqueId();
 
     const promise = axios.get(correctUrl)
@@ -43,40 +95,6 @@ const app = () => {
         checkUpdate(correctUrl, feedID);
       });
     return promise;
-  };
-
-  const checkUpdate = (url, feedID) => {
-    const parser = new DOMParser();
-    const correctUrl = `https://cors-anywhere.herokuapp.com/${url}`;
-    axios.get(correctUrl)
-      .then((response) => {
-        const data = parser.parseFromString(response.data, 'text/xml');
-        const items = data.querySelectorAll('channel item');
-        const checkLink = [];
-        [...items].forEach((singleElement) => {
-          const singleElementLink = singleElement.querySelector('link').textContent;
-          checkLink.push(singleElementLink);
-          const actualPost = watchedState.posts.filter((post) => post.feedId === feedID);
-          const alreadyAddLink = [];
-          actualPost.forEach((post) => alreadyAddLink.push(post.link));
-          checkLink.forEach((link) => {
-            const isNewLink = alreadyAddLink.includes(link);
-            if (!isNewLink) {
-              const singleElementTitle = singleElement.querySelector('title').textContent;
-              watchedState.posts.push({
-                id: _.uniqueId(),
-                feedId: feedID,
-                title: singleElementTitle,
-                link: singleElementLink,
-              });
-              watchedState.form.state = 'have update';
-              watchedState.form.state = '';
-              console.log(watchedState);
-            }
-          });
-        });
-      });
-    setTimeout(() => checkUpdate(url, feedID), 5000);
   };
 
   form.addEventListener('submit', (event) => {
